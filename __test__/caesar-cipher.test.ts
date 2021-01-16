84964035e73db8bcdd9b16c84e232ea09aa34558
elianiva <dicha.arkana03@gmail.com>
2021-01-16 21:16:01 +0700

    feat: added ceasar cipher algorithm

---
+++ __test__/caesar-cipher.test.ts
@@ -0,0 +1,50 @@
+import { encrypt, decrypt } from "../algo/caesar-cipher"
+
+const RAW = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+const RAW_LOWER = "the quick brown fox jumps over the lazy dog"
+
+describe("> Caesar Cipher", () => {
+  describe("#uppercase", () => {
+    test("should be able to encrypt string", () => {
+      expect(encrypt(RAW, 3)).toBe(
+        "WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ"
+      )
+    })
+
+    test("should be able to encrypt with more than 13 shift", () => {
+      expect(encrypt(RAW, 14)).toBe(
+        "HVS EIWQY PFCKB TCL XIADG CJSF HVS ZONM RCU"
+      )
+    })
+
+    test("should decrypt just fine", () => {
+      expect(decrypt(encrypt(RAW, 14), 14)).toBe(RAW)
+    })
+
+    test("should decrypt with stupid amount of key", () => {
+      expect(decrypt(encrypt(RAW, 10000), 10000)).toBe(RAW)
+    })
+  })
+
+  describe("#lowercase", () => {
+    test("should be able to encrypt string", () => {
+      expect(encrypt(RAW_LOWER, 3)).toBe(
+        "wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj"
+      )
+    })
+
+    test("should be able to encrypt with more than 13 shift", () => {
+      expect(encrypt(RAW_LOWER, 14)).toBe(
+        "hvs eiwqy pfckb tcl xiadg cjsf hvs zonm rcu"
+      )
+    })
+
+    test("should decrypt just fine", () => {
+      expect(decrypt(encrypt(RAW_LOWER, 14), 14)).toBe(RAW_LOWER)
+    })
+
+    test("should decrypt with stupid amount of key", () => {
+      expect(decrypt(encrypt(RAW_LOWER, 10000), 10000)).toBe(RAW_LOWER)
+    })
+  })
+})
---
+++ algo/caesar-cipher.ts
@@ -0,0 +1,49 @@
+/**
+ * True `modulo` in math, why Javascript doesn't have this
+ *
+ * @param {number} x - LHS number
+ * @param {number} y - RHS number
+ * @return number
+ */
+const mod = (x: number, y: number): number => x - y * Math.floor(x / y)
+
+/**
+ * Returns an encrypted string
+ * @param {string[]} rawStr - String
+ * @return (number|string)[]
+ */
+export const encrypt = (raw: string, shift: number): string => {
+  return raw
+    .split("")
+    .map((char: string) => {
+      const charCode = char.charCodeAt(0)
+      const asciiShift = char === char.toUpperCase() ? 65 : 97
+      return char === " "
+        ? " "
+        : String.fromCharCode(
+            mod(charCode - asciiShift + shift, 26) + asciiShift
+          )
+    })
+    .join("")
+}
+
+/**
+ * Returns a decrypted string
+ * @param {string[]} rawStr - String
+ * @return (number|string)[]
+ */
+export const decrypt = (raw: string, shift: number): string => {
+  return raw
+    .split("")
+    .map((char: string) => {
+      const charCode = char.charCodeAt(0)
+      const asciiShift = char === char.toUpperCase() ? 65 : 97
+      // 66 - 97 = -31 - 13 = -44 % 26 = 18 + 97 = 125
+      return char === " "
+        ? " "
+        : String.fromCharCode(
+            mod(charCode - asciiShift - shift, 26) + asciiShift
+          )
+    })
+    .join("")
+}
