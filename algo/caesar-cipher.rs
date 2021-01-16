f855e3ba2a4c3a3100ce5fc7df27333cf2973cb6
elianiva <dicha.arkana03@gmail.com>
2021-01-16 22:36:06 +0700

    feat: added caesar cipher in rust cuz y not

---
+++ algo/caesar-cipher.rs
@@ -0,0 +1,54 @@
+use std::char;
+
+fn main() {
+    // const RAW: &str = "THE BROWN FOX JUMPS OVER THE LAZY DOG";
+    const RAW: &str = "tHiS iS An unNEcesSarYLy loNg SenTeNCe tO TeST iF my CodE WOrkS";
+    const SHIFT: i32 = 10;
+
+    let encrypted = encrypt(RAW, SHIFT);
+
+    println!("{}", encrypted);
+    println!("{}", decrypt(&encrypted[..], SHIFT));
+}
+
+fn modulo(x: i32, y: i32) -> u32 {
+    ((x % y + y) % y) as u32
+}
+
+fn encrypt(data: &str, shift: i32) -> String {
+    data.chars()
+        .map(|letter| {
+            let char_code = letter as i32;
+            let is_uppercase = &letter.to_string() == &letter.to_uppercase().to_string();
+            let ascii_shift = if is_uppercase { 65 } else { 97 };
+
+            if letter == ' ' {
+                ' '
+            } else {
+                char::from_u32(
+                    modulo(char_code - ascii_shift + shift, 26 as i32) + ascii_shift as u32,
+                )
+                .unwrap()
+            }
+        })
+        .collect::<String>()
+}
+
+fn decrypt(data: &str, shift: i32) -> String {
+    data.chars()
+        .map(|letter| {
+            let char_code = letter as i32;
+            let is_uppercase = &letter.to_string() == &letter.to_uppercase().to_string();
+            let ascii_shift = if is_uppercase { 65 } else { 97 };
+
+            if letter == ' ' {
+                ' '
+            } else {
+                char::from_u32(
+                    modulo(char_code - ascii_shift - shift, 26 as i32) + ascii_shift as u32,
+                )
+                .unwrap()
+            }
+        })
+        .collect::<String>()
+}
